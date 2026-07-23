69ba0171564207daaea02ce9225ddd257144824f
spamuse <sangyeon@zeeps.net>
2026-07-23 16:09:29 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
-from .unified_book import UnifiedBookMetadataProvider
 
-# BookOasis 메인 시스템이 플러그인을 인식하도록 클래스를 반환하거나 내보냄
+# unified_book.py에 정의된 원본 클래스를 안전하게 임포트
+try:
+    from .unified_book import UnifiedBookMetadataProvider
+except ImportError:
+    from unified_book import UnifiedBookMetadataProvider
+
+# BookOasis 메인 시스템이 플러그인 클래스를 로드할 때 호출하는 표준 진입점
 def get_plugin_class():
     return UnifiedBookMetadataProvider
 
-# 일부 버전의 BookOasis 호환성을 위해 클래스 자체를 직접 노출
+# 일부 메인 시스템 버전 호환성을 위한 별칭 등록
 Plugin = UnifiedBookMetadataProvider
