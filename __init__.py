43ce0c0f6857fec2d17378b2863edc83f7d9d680
spamuse <sangyeon@zeeps.net>
2026-07-23 17:19:59 +0900

    Clean up __init__.py by removing redundant code
    Remove unused function and variable assignments.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,2 @@
 # -*- coding: utf-8 -*-
 from .unified_book import UnifiedBookMetadataProvider
-
-def get_plugin_class():
-    return UnifiedBookMetadataProvider
-
-Plugin = UnifiedBookMetadataProvider
